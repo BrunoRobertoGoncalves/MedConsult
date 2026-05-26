e559f4150e2251a3a15c9c10c14ccae39ab8dff8
Bruno Roberto Goncalves da Silva <bruno.goncalves@programmers.com.br>
2026-05-26 18:48:25 -0300

    feat: add ApplicatioDataContext with IUnitOfWork implementation

---
+++ Domain/Core/Data/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Core.Data
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        Task<int> SaveChangesAsync();
+        Task<bool> SaveEntitiesAsync();
+    }
+}
---
+++ Infraestructure/Data/ApplicationDataContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infraestructure.Data
+{
+    public class ApplicationDataContext : DbContext, IUnitOfWork
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : base(options)
+        {
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+
+        public async Task<bool> SaveEntitiesAsync()
+        {
+            await SaveChangesAsync();
+            return true;
+        }
+    }
+}
