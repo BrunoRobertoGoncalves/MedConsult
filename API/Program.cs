b53a97e7221511fd7e2cb681e977d6f5dafc75ac
Bruno Roberto Goncalves da Silva <bruno.goncalves@programmers.com.br>
2026-07-23 11:40:55 -0300

    Some fixes that i only saw now

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,8 @@
+using Domain.AggregatesModel.ClinicalCaseAggregate.Repository;
+using Domain.AggregatesModel.ExamAggregate.Repository;
+using Domain.AggregatesModel.UserAggregate.Repository;
 using Infraestructure.Data;
+using Infraestructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,8 +14,13 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApplicationDataContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-var app = builder.Build();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
 
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IClinicalCaseRepository, ClinicalCaseRepository>();
+builder.Services.AddScoped<IExamRepository, ExamRepository>();
+
+var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
